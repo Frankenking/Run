d07a432aaf160f13fad269222e2452acac0af82b
Frankenking <99056962+Frankenking@users.noreply.github.com>
2023-02-06 18:14:33 -0700

    e

---
+++ scenes/TimeLabel.gd
@@ -0,0 +1,18 @@
+extends Label3D
+
+var currentTime: float = 0.0
+var finished: bool = false
+
+	
+func _ready():
+	pass
+	
+func _process(delta):
+	if finished:
+		pass
+	else:
+		if $"../Stopwatch".time_left <= 0:
+			$"../Stopwatch".start(0.1)
+			currentTime += 0.1
+			$".".set_text("Time: " + String(currentTime))
+			
